efacca7cd5f24f2ffd30de659432311103ccdeae
sarayuramya <ramyaks1996@gmail.com>
2017-03-14 11:12:02 +0530

    Update formrules.c

--- src/formrules.c
+++ src/formrules.c
@@ -361,8 +361,9 @@ void PopCondition()
 /*	ruleset if it is sufficiently accurate				 */
 /*									 */
 /*************************************************************************/
-
-#define TI(a,b)		(((a)+(b)) * Log((a)+(b)) - (a) * Log(a) - (b) * Log(b))
+#define alpha 1.95
+#define q 1/(1-alpha)
+#define TI(a,b)		((Log(pow((a+b),alpha))*q) -( Log(pow(a,alpha))*q) - (Log(pow(b,alpha)))*q))
 
 
 void PruneRule(Condition Cond[], ClassNo TargetClass)
